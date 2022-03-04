2b95361bcf5777bb8a83b916fb456fd52e3ee2b3
Debra Shelby <deborashelby@gmail.com>
2022-03-03 22:35:41 -0500

    first commit

---
+++ switch.py
@@ -0,0 +1,39 @@
+# creating a function for switch statement
+def switch(case):
+
+   # checks if the argument matches first case
+   if case == 1:
+       print("Monday")
+
+   # checks if the argument matches second case
+   elif case == 2:
+       print("Tuesday")
+
+   # checks if the argument matches third case
+   elif case == 3:
+       print("Wednesday")
+
+   # checks if the argument matches fourth case
+   elif case == 4:
+       print("Thursday") 
+
+   # checks if the argument matches fifth case
+   elif case == 5:
+       print("Friday") 
+
+   # checks if the argument matches sixth case
+   elif case == 6:
+       print("Saturday")
+        
+
+   # checks if the argument matches seventh case
+   elif case == 7:
+       print("Sunday") 
+
+   else:
+       print("Please enter number between 1-7")
+
+#calling function
+
+# switch(4)
+switch(9)
